3d7d27dc86154dacf5d8cfd410cdbfdaa8d2a882
David Epps <depps@Davids-MacBook-Air.local>
2018-07-26 16:29:22 -0600

    DE randmovie2 complete 2

--- controllers/movieSelector.js
+++ controllers/movieSelector.js
@@ -1,20 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const request = require('request');
 
 const Movies = require("../models/movies");
 
 
 
-router.get("/", (req, res) => {
-	Movies.find({}, (err, allMovies) => {
-		if(err){
-			console.log(err)
-			res.send("err")
-		} else {
-			res.render("movieSelector/home")
-		}
-	})
-});
+
 
 router.get('/pick', (req,res) => {
 	request('https://api.themoviedb.org/3/genre/movie/list?api_key=32d3d0cbaf9018e6f6e161c4581015e6&language=en-US', (error, response, body) => {
@@ -22,11 +14,23 @@ router.get('/pick', (req,res) => {
   		console.log(error)
   	} else {
 	var genreObj = JSON.parse(body);
-	res.render('movieSelectort/home', {
+	console.log(genreObj)
+	res.render('movieSelector/home.ejs', {
 		genreSelect:genreObj
-	})
+	});
 }
 })
 })
+
+router.get("/", (req, res) => {
+	Movies.find({}, (err, allMovies) => {
+		if(err){
+			console.log(err)
+			res.send(err)
+		} else {
+			res.render("movieSelector/home.ejs")
+		}
+	})
+});
 
 module.exports=router;
