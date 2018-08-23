66ca0ae0aec23f7606ce910e3df098e2dc332af0
Patrick Gray <patrickgray@Patricks-MacBook-Pro.local>
2018-08-23 11:59:23 -0600

    put stuff back in

--- server.js
+++ server.js
@@ -39,21 +39,21 @@ require("dotenv").config();
 //   assert.ok(false);
 // });
 
-// require('./db/db'); 
+require('./db/db'); 
 
-// ///////setup sessions
-// app.use(require('express-session')({
-//   secret: 'This is a secret',
-//   cookie: {
-//     maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
-//   },
-//   store: store,
-//   // Boilerplate options, see:
-//   // * https://www.npmjs.com/package/express-session#resave
-//   // * https://www.npmjs.com/package/express-session#saveuninitialized
-//   resave: true,
-//   saveUninitialized: true
-// }));
+///////setup sessions
+app.use(require('express-session')({
+  secret: 'This is a secret',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+  },
+  store: store,
+  // Boilerplate options, see:
+  // * https://www.npmjs.com/package/express-session#resave
+  // * https://www.npmjs.com/package/express-session#saveuninitialized
+  resave: true,
+  saveUninitialized: true
+}));
 
 //////////////passport middleware//////////////////
 app.use(passport.initialize());
