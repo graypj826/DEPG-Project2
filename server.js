7fbfe8eb00677dacf4ed13682fbe6944a3547499
Patrick Gray <patrickgray@Patricks-MacBook-Pro.local>
2018-08-23 10:15:08 -0600

    revert to old port settings

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const assert        = require("assert");
 
 ////require database, .env and passport
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000 || 7000;
 
 require("dotenv").config();
  
@@ -110,10 +110,7 @@ app.get('/landing', (req,res) => {
 
 ////set up launch///////////////////////////////////
 
-// app.listen(port);
-// console.log('---------------------------------');
-// console.log('Server running on port: ' + port);
-// console.log('---------------------------------');
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-})
+app.listen(port);
+console.log('---------------------------------');
+console.log('Server running on port: ' + port);
+console.log('---------------------------------');
